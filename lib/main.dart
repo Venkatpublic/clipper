6c61d2ec339d23b0023ff38356768a370d51cde3
Venkat <venkatasalam0412@gmail.com>
2026-07-14 19:17:38 +0530

    feat: base UI IP

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
+import 'package:clipper/src/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Clipper"),
-          centerTitle: true,
-          backgroundColor: const Color(0xFF6D5DF6),
-        ),
-        body: Text('Hello there!'),
-      ),
-    ),
-  );
+  runApp(const MaterialApp(home: Home()));
 }
---
+++ lib/src/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Clipper"),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF6D5DF6),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            color: Colors.red,
+            width: 50,
+            child: Text('One'),
+            alignment: Alignment.center,
+          ),
+          Container(
+            color: Colors.indigo,
+            width: 100,
+            child: Text('Two'),
+            alignment: Alignment.center,
+          ),
+          Container(
+            color: Colors.grey,
+            width: 200,
+            child: Text('Three'),
+            alignment: Alignment.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
