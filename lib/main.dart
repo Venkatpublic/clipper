4ea10f5b7d35de33e01067788363cdc52805091e
Venkat <venkatasalam0412@gmail.com>
2026-07-16 20:12:48 +0530

    LR:quiz app started

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,27 @@
-import 'package:clipper/gradient_container.dart';
-import 'package:clipper/src/screens/splash_screen.dart';
-import 'package:clipper/src/theme/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer.purple())));
-  // runApp(const AutoCropApp());
-}
-
-class AutoCropApp extends StatelessWidget {
-  const AutoCropApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AutoCrop',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          primary: AppColors.primary,
-          onPrimary: AppColors.onPrimary,
-          secondary: const Color(0xFF505F76),
-          onSecondary: Colors.white,
-          error: AppColors.error,
-          onError: Colors.white,
-          surface: AppColors.surface,
-          onSurface: AppColors.onSurface,
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/quiz-logo.png', height: 300, width: 300),
+              SizedBox(height: 50),
+              Text(
+                "Learn Flutter The Fun Way!",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              SizedBox(height: 50),
+              OutlinedButton(onPressed: () {}, child: Text('Start Quiz')),
+            ],
+          ),
         ),
-        textTheme: GoogleFonts.interTextTheme(),
-        scaffoldBackgroundColor: AppColors.surface,
       ),
-      home: const SplashScreen(),
-    );
-  }
+    ),
+  );
+  // runApp(const AutoCropApp());
 }
