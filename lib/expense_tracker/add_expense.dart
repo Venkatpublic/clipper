19749e683c645e0793bc69e09ce97a13d69ccd93
Venkat <venkatasalam0412@gmail.com>
2026-07-24 14:47:32 +0530

    update:expense app IP

--- lib/expense_tracker/add_expense.dart
+++ lib/expense_tracker/add_expense.dart
@@ -1,3 +1,4 @@
+import 'package:clipper/expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class AddExpense extends StatefulWidget {
@@ -10,11 +11,26 @@ class AddExpense extends StatefulWidget {
 class _AddExpenseState extends State<AddExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  Category selectedCat = Category.leisure;
+  DateTime? selectedDate;
   @override
   void dispose() {
     _titleController.dispose();
     _amountController.dispose();
     super.dispose();
+  }
+
+  void _opendateModal() async {
+    DateTime now = DateTime.now();
+    DateTime start = DateTime(now.year - 1, now.month, now.day);
+    var userChoice = await showDatePicker(
+      context: context,
+      firstDate: start,
+      lastDate: now,
+    );
+    setState(() {
+      selectedDate = userChoice;
+    });
   }
 
   @override
@@ -28,15 +44,59 @@ class _AddExpenseState extends State<AddExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
           ),
-          TextField(
-            controller: _amountController,
-            decoration: const InputDecoration(
-              label: Text('Title'),
-              prefixText: '\$',
-            ),
-          ),
+
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  decoration: const InputDecoration(
+                    label: Text('Title'),
+                    prefixText: '\$',
+                  ),
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      selectedDate == null
+                          ? 'No Date Selected'
+                          : formatter.format(selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _opendateModal,
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              DropdownButton(
+                value: selectedCat,
+                items: Category.values.map((cat) {
+                  return DropdownMenuItem(
+                    child: Text(cat.name.toUpperCase()),
+                    value: cat,
+                  );
+                }).toList(),
+                onChanged: (val) {
+                  if (val == null) {
+                    return;
+                  }
+                  setState(() {
+                    selectedCat = val;
+                  });
+                },
+              ),
+              Spacer(),
               ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
